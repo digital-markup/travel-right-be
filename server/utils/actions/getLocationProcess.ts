9711835a15f1fa3af6af5db6b05769994883b624
harshawije-dev <zettatechlk@gmail.com>
2024-12-04 03:29:03 +0530

    update the file2222

--- server/utils/actions/getLocationProcess.ts
+++ server/utils/actions/getLocationProcess.ts
@@ -12,16 +12,17 @@ export const getLocationProcess = async ({ start, end }: GeoCode): Promise<any> 
         if (!response) {
             return "An error occurred while processing your request.";
         }
-        // // get the direction & pollyline data
-        // const polyLineData = await listPollyLines({
-        //     departure: response.departure,
-        //     destination: response.destination
-        // });
-        // const points = decodePollyLines(polyLineData);
-        // if (points.length === 0) return "An error occurred while processing your request.";
+        // get the direction & pollyline data
+        const polyLineData = await listPollyLines({
+            departure: response.departure,
+            destination: response.destination
+        });
+        const points = decodePollyLines(polyLineData);
+        if (points.length === 0) return "An error occurred while processing your request.";
 
         return {
             coordinates: [response.departure, response.destination],
+            points: releasePoints(points),
         };
     } catch (error) {
         return "An error occurred while processing your request." + error;

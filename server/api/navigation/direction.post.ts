cb633e6b1706b5f165564c5d3de9959f99ae3be8
harshawije-dev <zettatechlk@gmail.com>
2024-12-04 03:10:50 +0530

    route changed to body

--- server/api/navigation/direction.post.ts
+++ server/api/navigation/direction.post.ts
@@ -2,16 +2,16 @@ import { getLocationProcess } from "~/utils/actions/getLocationProcess"
 
 export default defineEventHandler(async (event) => {
     try {
-        const query = getQuery(event)
-        if (!query) {
+        const body = await readBody(event)
+        if (!body || body === undefined) {
             return {
-                message: "Error: No query found",
+                message: "Error: No body found",
                 statusCode: 400,
                 body: null,
                 success: false
             } as HTTPResponse
         }
-        const { start, end } = query;
+        const { start, end } = body;
         const response = await getLocationProcess({ start: start.toLocaleString(), end: end.toLocaleString() });
         
         return {

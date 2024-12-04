1eee3df2a66bb0cc2d6eac4cb847910a87bb0755
harshawije-dev <zettatechlk@gmail.com>
2024-12-04 11:16:45 +0530

    update the template

--- server/utils/services/templates.ts
+++ server/utils/services/templates.ts
@@ -5,6 +5,7 @@ When the question is not related to the context, politely respond that you are t
 If you cannot find the answer, politely respond that you are not updated with that information and provide some information about the context through web search. 
 DO NOT try to make up an answer.
 If the question is about a certain route, just provide the landmarks for that route through web search and nothing else. 
+When the user ask for create him a package, provide him a list of locations which amazing to visit in Sri Lanka.
 Finally if your response contains location names, provide them in a json object with the key 'location' and the value as the location name at the end of the response separately.
 
 {context}

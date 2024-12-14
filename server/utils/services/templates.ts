a763f0b0884a6e40f2a3e2e3007c893e5627d928
harshawije-dev <zettatechlk@gmail.com>
2024-12-14 17:07:22 +0530

    prompt update

--- server/utils/services/templates.ts
+++ server/utils/services/templates.ts
@@ -1,17 +1,22 @@
 // Creates a standalone question from the chat-history and the current question
-const BASE_TEMPLATE = `You are an enthusiastic AI assistant. Use the following pieces of context to answer the question at the end.
-If user greets you, politely greet back the user. 
-When the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. 
-If you cannot find the answer, politely respond that you are not updated with that information and provide some information about the context through web search. 
-DO NOT try to make up an answer.
-If the question is about a certain route, just provide the landmarks for that route through web search and nothing else. 
-When the user ask for create him a package, provide him a list of locations which amazing to visit in Sri Lanka.
-Finally if your response contains location names, provide them in a json object with the key 'location' and the value as the location name at the end of the response separately.
+const BASE_TEMPLATE = `Classify the user's question into one of the following categories: [Places recommendation, Tour Packages, Budget Estimation].
+Examples:
+Question: "What are the best places to visit in Sri Lanka?"
+Classification: Places recommendation
+
+Question: "I like to enjoy the wildlife, do surfing and hiking. Can you suggest some places to visit?"
+Classification: Tour Packages
+
+Question: "How much does it cost to visit ?"
+Classification: Budget Estimation
 
 {context}
 
-Question: {question}
-Helpful answer in markdown:`;
+Now classify the following:
+Question: "{question}"
+Classification:
+Helpful answer in markdown:
+`;
 
 // Refers to the history of previous questions and answers
 const STANDALONE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
